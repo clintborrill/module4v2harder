add2d56b27f6524413768db54c94ac5f0695ec62
clintborrill <40843647+clintb@users.noreply.github.com>
2019-12-15 13:00:12 -0800

    reverted console log

--- script.js
+++ script.js
@@ -61,9 +61,9 @@ for (var i = 0; i < names.length; i ++ ) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter = "j"/* fill in condition here */) {
-    console.log(byeSpeaker.speak() + names);
+    byeSpeaker.speak(names[i]);
   } else {
-    console.log(helloSpeaker.speak() + names);
+    helloSpeaker.speak(names[i]);
   }
 }
 })();
